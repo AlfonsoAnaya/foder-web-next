b3563d2e191e02b54e657145265e53c0c1d4038a
Alfonso Anaya <alfonso.ws@gmail.com>
2024-03-11 15:25:18 +0100

    load one component to page

---
+++ app/components/Header/MobileNavMenu.tsx
@@ -0,0 +1,49 @@
+'use client'
+
+function MobileNavMenu(
+    props: { toggleMobileNav: () => void }
+) {
+    return (
+        <div className="nav-toggle flex self-center
+                    fixed top-[57px] left-0 right-0 md:static
+                    w-[100%] h-[calc(100vh)] md:w-auto md:h-auto 
+                    translate-x-full md:translate-x-0
+                    bg-primary md:bg-transparent
+                    justify-center md:justify-start"
+        >
+            <ul className="flex gap-[2rem] font-[600] uppercase
+                        pt-40 md:pt-0
+                        text-white 
+                        flex-col md:flex-row
+                        items-center md:items-start
+                        text-[1.75em] md:text-[1.05em]"
+            >
+                {/* <Link to={`/`}>
+                    <li
+                        onClick={props.toggleMobileNav}
+                        className="pb-[2px] hover:text-secondary cursor-pointer border-b-[transparent] border-b-[3px] 
+                                hover:border-b-secondary hover:border-b-[3px] transition-fill duration-300">
+                        Recetas
+                    </li>
+                </Link> */}
+                
+                    <li
+                        onClick={props.toggleMobileNav}
+                        className="hover:text-secondary hover:cursor-pointer hover:underline hover:underline-offset-8 decoration-[3px]">
+                        Plan Semanal
+                    </li>
+                
+
+                
+                    <li
+                        onClick={props.toggleMobileNav}
+                        className="hover:text-secondary hover:cursor-pointer hover:underline hover:underline-offset-8 decoration-[3px]">
+                        Lista de Compras
+                    </li>
+                
+            </ul>
+        </div>
+    )
+}
+
+export default MobileNavMenu
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,26 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        primary: "rgb(36, 131, 123)",
+        secondary: "rgb(218, 112, 44)",
+        tertiary: "#fcba03",
+        quatertiary: "#d15036",
+        btn: "#68d463e8",
+        white: "rgb(255, 253, 252)",
+        dark: "rgb(54, 54, 51)",
+        darker: "rgb(16, 15, 15)",
+        beige: "rgb(240, 239, 235)",
+        grayLight: "rgb(243, 244, 246)",
       },
+      fontFamily: {
+        siteTitle: "'Tilt Neon', sans-serif",
+        // primary: "'Josefin Sans', sans-serif",
+        sans: "'Raleway', sans-serif",
+        // primary: "'Jost', sans-serif",
+        serif: "'Lora', serif",
+        tertiary: "sans-serif"
+      }
     },
   },
   plugins: [],
