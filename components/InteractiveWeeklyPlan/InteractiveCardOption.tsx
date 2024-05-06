524821391662d88c49b8710204340027efd20d33
Alfonso Anaya <alfonso.ws@gmail.com>
2024-05-06 22:19:52 +0200

    customize new version

--- components/InteractiveWeeklyPlan/InteractiveCardOption.tsx
+++ components/InteractiveWeeklyPlan/InteractiveCardOption.tsx
@@ -1,5 +1,6 @@
-import Recipe from "@/types/recipe.d"
-import Image from "next/image"
+import Recipe from "@/types/recipe.d";
+import Image from "next/image";
+import { RxLapTimer } from "react-icons/rx";
 
 interface InteractiveCardOptionProps {
     recipe: Recipe
@@ -13,14 +14,13 @@ function InteractiveCardOption(
     const isVegetarian = false
 
     return (
-
-        <div className="card-option flex justify-center align-center md:max-w-[150px] border-[1px] border-gray-400 hover:border-primary bg-gray_2 rounded-lg cursor-pointer">
-            <div className="recipe-card max-w-[330px] h-[530px] md:max-h-[220px] flex flex-col">
+        <div className="card-option flex w-[120px] md:max-w-[155px] h-[200px] md:h-[530px] md:max-h-[220px] justify-center align-center border-[1px] border-gray-400 hover:border-primary bg-gray_2 rounded-lg cursor-pointer">
+            <div className="recipe-card flex flex-col w-[100%]">
 
                 {/* RECIPE IMAGE */}
-                <div className="relative recipe-img w-[100%] h-[90%] md:h-[60%] rounded-t-lg">
+                <div className="relative recipe-img w-[100%] h-[60%] rounded-t-lg">
                     <Image
-                        className="block object-cover w-[100%] h-[100%] object-center rounded-t-lg"
+                        className="block object-cover w-[100%] h-[100%] object-center rounded-t-[7px]"
                         src={`/images/recipes/${recipe.img}`}
                         // alt={recipe.imgAlt} 
                         alt="un plato de comida"
@@ -40,15 +40,19 @@ function InteractiveCardOption(
                 </div>
 
                 {/* RECIPE INFO */}
-                <div className="recipe-info h-[10%] md:h-[50%] flex flex-col gap-2 justify-center p-2 bg-transparent">
-                    <h4 className="recipe-title text-[.8rem] text-gray-600 font-[600]">
-                        {recipe.name} con salsa de soya
+                <div className="recipe-info h-[10%] md:h-[50%] flex flex-col gap-2 justify-start p-2 bg-transparent">
+                    <h4 className="recipe-title text-[.8rem] text-dark font-[600]">
+                        {recipe.name}
                     </h4>
-                    {/* <div className="small-info flex flex-row gap-4 text-secondary text-[.85rem] font-[500]">
-                        <span >
-                            {recipe.difficulty}
-                        </span>
-                    </div> */}
+                    <div className="small-info font-sans flex flex-wrap flex-row  gap-1 md:gap-2 text-darker text-[.65rem] font-[600]
+                            [&>*]:bg-gray-200 [&>*]:py-[.3em] [&>*]:px-[.7em] [&>*]:rounded-sm">
+                            {/* <span className="flex gap-1 justify-center items-center">
+                                <RxLapTimer className="text-[1rem]" /> total: {recipe.totalTime} min.
+                            </span> */}
+                            <span className="flex gap-1 justify-center items-center">
+                                <RxLapTimer className="text-[1rem]" /> prep: {recipe.prepTime} min.
+                            </span>
+                        </div>
                 </div>
             </div>
         </div>
