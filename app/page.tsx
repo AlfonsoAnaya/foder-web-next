234dc4971d1fbc0320ded5855da19f9d70deeb55
Alfonso Anaya <alfonso.ws@gmail.com>
2024-06-16 15:53:25 +0200

    test osano script

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,16 @@
-import Header from "../components/Header/Header";
 import HomePage from "../components/Home/HomePage";
-import Footer from "../components/Footer/Footer";
+import Script from "next/script";
 
 export default function Home() {
   return (
+    <>
+    <Script src="https://cmp.osano.com/AzZZBuUFrPnYHIGKQ/b1ba713a-9737-4d07-a592-6ce8532b0171/osano.js" />
     <main className="flex flex-col min-h-screen font-sans w-[100%] bg-white">
       {/* <Header /> */}
       <HomePage />
       {/* <Footer /> */}
     </main>
+    </>
+    
   );
 }
