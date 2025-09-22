310da1c0aa161f384576e772153c23198d6129c9
Alfonso Anaya <alfonso.ws@gmail.com>
2025-09-22 23:01:25 +0200

    update css and toggle function

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,9 +1,10 @@
 'use client'
 //import SearchBar from "./Shared/SearchBar";
 import { usePathname } from "next/navigation";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import Image from "next/image";
 import MobileNavMenu from "./MobileNavMenu";
+import styles from "./Header.module.scss";
 import "./Header.css";
 import Link from "next/link";
 import useCurrentNavSectionStore from "@/app/ZustandStore/CurrentNavSectionStore";
@@ -21,60 +22,58 @@ function Header() {
     ) hideBorder = true;
 
     if (pathname === "/plan-actual/lista-de-compras"
-    || pathname === "/plan-actual-vegtariano/lista-de-compras"
+        || pathname === "/plan-actual-vegtariano/lista-de-compras"
     ) changeColor = true;
 
+    const [isOpen, setIsOpen] = useState(false);
 
     const toggleMobileNav = () => {
-        const navToggle = document.querySelector('.header-nav');
-        if (navToggle?.classList.contains("nav-open")) navToggle?.classList.remove("nav-open")
-        else {navToggle?.classList.add("nav-open")}
-    }
+    setIsOpen((prev) => !prev);
+    };
 
     const closeMobileNav = () => {
-        const navToggle = document.querySelector('.header-nav');
-        navToggle?.classList.remove("nav-open");
-    }
+        setIsOpen(false)
+    };
 
     const updateCurrentNavSection = useCurrentNavSectionStore((state) => state.updateCurrentNavSection);
 
     return (
-        <header className={`sticky header-nav top-0 z-20  w-100% flex justify-center align-center px-4 md:px-0 nav: border-b-dark border-b-[1px] border-dashed
-            bg-white ${hideBorder ? "border-none" : ""} ${changeColor ? "background-list": ""}`}>
+
+        <header className={`${styles.header} header-nav ${isOpen ? "nav-open" : ""} ${hideBorder ? "border-none" : ""} first-letter:${changeColor ? "background-list" : ""}`}>
             <div className="flex flex-col justify-center align-center w-[90%] max-w-[1200px]">
                 <div className="flex flex-1 flex-row md:gap-16 items-center justify-between relative">
 
                     {/* Website Name */}
-                    
-                        <h1 id="header-title"
-                            className="large-text text-left  
+
+                    <h1 id="header-title"
+                        className="large-text text-left  
                             font-siteTitle
                             text-dark uppercase
                             text-[2.5em] md:text-[3.15em] leading-[1em] py-[12px]"
-                            onClick={() => {
-                                closeMobileNav;
-                                updateCurrentNavSection("");
-                            }}>
-                            <Link 
-                                href="/"
-                                onClick={closeMobileNav}
-                            >
-                                <Image 
-                                    src="/images/logos/logo-dark.svg"
-                                    width="200"
-                                    height="50"
-                                    alt="logo pipián"
-                                />
-                            </Link>
-                            
-                        </h1>
-                    
+                        onClick={() => {
+                            closeMobileNav;
+                            updateCurrentNavSection("");
+                        }}>
+                        <Link
+                            href="/"
+                            onClick={closeMobileNav}
+                        >
+                            <Image
+                                src="/images/logos/logo-dark.svg"
+                                width="200"
+                                height="50"
+                                alt="logo pipián"
+                            />
+                        </Link>
+
+                    </h1>
+
                     <MobileNavMenu
-                    toggleMobileNav={toggleMobileNav}
+                        toggleMobileNav={toggleMobileNav}
                     />
 
                     {/* <HeaderUtilities /> */}
-                    
+
                     <div className="block md:hidden"
                         onClick={toggleMobileNav}>
                         <span className="hamburger bg-dark">
